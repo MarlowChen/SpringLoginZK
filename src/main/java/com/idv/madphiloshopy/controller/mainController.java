95dcd53630c298f04eacb91e0c140a3b37da7a2e
MarlowChen <mac@madphilosophy.idv.tw>
2017-02-05 14:41:55 +0800

    [Modify] edit jdni and zul

--- src/main/java/com/idv/madphiloshopy/controller/mainController.java
+++ src/main/java/com/idv/madphiloshopy/controller/mainController.java
@@ -1,9 +1,24 @@
 package com.idv.madphiloshopy.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
 
 @Controller
-public class mainController {
+public class mainController extends GenericForwardComposer {
 	
 	
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+             
+       
+    }
+    
+    
 }
